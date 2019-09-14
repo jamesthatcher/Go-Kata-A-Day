090c3d46c217428d9333d3567948ffbc846a5069
James Thatcher <james.e.thatcher@pwc.com>
2019-09-14 09:27:05 +0100

    Initial commit

---
+++ longest_vowel_chain/solve.go
@@ -0,0 +1,15 @@
+package longest_vowel_chain
+
+import "fmt"
+
+// The vowel substrings in the word codewarriors are o,e,a,io.
+// The longest of these has a length of 2. Given a lowercase string that
+// has alphabetic characters only and no spaces, return the length of the
+// longest vowel substring.
+
+func Solve(s string) int {
+	for pos, char := range s {
+		fmt.Println(char, pos)
+	}
+	return 0
+}
