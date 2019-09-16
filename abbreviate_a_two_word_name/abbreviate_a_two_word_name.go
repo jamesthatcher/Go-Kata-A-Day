f44260a1d7942239c20b372dc828cdca0b50ca51
James Thatcher <james.e.thatcher@pwc.com>
2019-09-16 09:38:00 +0100

    feat: added abbreviate a two world name challenge

---
+++ abbreviate_a_two_word_name/abbreviate_a_two_word_name.go
@@ -0,0 +1,6 @@
+package abbreviate_a_two_word_name
+
+func AbbrevName(name string) string {
+	//your code here
+	return ""
+}
---
+++ abbreviate_a_two_word_name/abbreviate_a_two_word_name_test.go
@@ -0,0 +1,24 @@
+package abbreviate_a_two_word_name
+
+import "testing"
+
+func TestAbbrevName(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"should test that the solution returns the correct value", args{"Sam Harris"}, "S.H"},
+		{"should test that the solution returns the correct value", args{"Patrick Feenan"}, "P.F"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AbbrevName(tt.args.name); got != tt.want {
+				t.Errorf("AbbrevName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
